2ec25f69757708aebc97e9de0044fe04e22b170e
Harry Tang <harry@maxspell.com>
2024-07-13 22:03:18 +0300

    fix: fixes s3 warning

--- config/plugins.ts
+++ config/plugins.ts
@@ -25,11 +25,11 @@ export default ({ env }) => ({
       providerOptions: {
         baseUrl: `https://${env("STORAGE_HOST")}`,
         rootPath: env("STORAGE_ROOT_PATH"),
-        credentials: {
-          accessKeyId: env("CF_ACCESS_KEY_ID"),
-          secretAccessKey: env("CF_ACCESS_SECRET"),
-        },
         s3Options: {
+          credentials: {
+            accessKeyId: env("CF_ACCESS_KEY_ID"),
+            secretAccessKey: env("CF_ACCESS_SECRET"),
+          },
           region: "auto",
           endpoint: env("CF_ENDPOINT"),
           params: {
