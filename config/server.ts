6e21899ebaaac3b014549566c4435b0e7d7ccbe6
Harry Tang <harry@maxspell.com>
2025-01-12 14:41:12 +0200

    chore: config proxy

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("STRAPI_ADMIN_BACKEND_URL", "http://localhost:1337"),
+  proxy: env.bool("IS_PROXIED", true),
   app: {
     keys: env.array("APP_KEYS"),
   },
